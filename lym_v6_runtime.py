28443981c42d4d485c5147138666d31181748a90
JonathanJosueGuardado <nathanjosue100@gmail.com>
2026-07-16 12:22:51 -0600

    Agregar fachada runtime LYM V6

---
+++ lym_v6_runtime.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+"""Fachada de integración LYM V6."""
+
+from lym_v6_runtime_base import *
+from lym_v6_runtime_base import (
+    _has_extended_date_permission, _to_float, _validate_dialog_dates,
+    _walk_dates, _report_output_dir,
+)
+from lym_v6_reports import *
